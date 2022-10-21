f5bbdd456c7c5f8e6dbd7853545de9a3337cecae
unknown <caglabircan@hotmail.com>
2022-10-22 00:36:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(name, event) {
+    const messages = []
+    for (let i = 0; i < messages.length; i++) {
+        messages.push(`Thank you, ${name}, for the wonderful ${event} gift!`)
+    }
+    return messages
+}
+
+function countDown(n) {
+    let countdown = n;
+    while (countdown > -1) {
+        console.log(countdown)
+        countdown--
+    }
+}
